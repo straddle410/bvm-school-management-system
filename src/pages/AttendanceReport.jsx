8ffba5c022cb4f0fec8c59268b74781815066359
Naresh G <straddle410@gmail.com>
2026-02-19 18:16:53 +0000

    File changes

--- src/pages/AttendanceReport.jsx
+++ src/pages/AttendanceReport.jsx
@@ -20,7 +20,7 @@ import {
 } from "@/components/ui/dialog";
 import { Eye, Phone } from 'lucide-react';
 
-const CLASSES = ['Nursery', 'LKG', 'UKG', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+const CLASSES = ['Nursery', 'LKG', 'UKG', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
 
 export default function AttendanceReport() {
   const [selectedClass, setSelectedClass] = useState('');
