e35c46df26c3f67f634bcc06650093d1330f5f77
Naresh G <straddle410@gmail.com>
2026-02-19 08:29:52 +0000

    File changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import {
   GraduationCap, Image, Calendar, Brain, Bell, MoreHorizontal,
-  ClipboardList, Megaphone, MessageCircle, ChevronRight, User
+  ClipboardList, Megaphone, MessageCircle, ChevronRight, User, BarChart3
 } from 'lucide-react';
 import { format } from 'date-fns';
 
