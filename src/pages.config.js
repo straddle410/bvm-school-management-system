5f7adea64127c0097c425daac9c6ecbb6312adbf
Naresh G <straddle410@gmail.com>
2026-02-19 18:31:34 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -52,7 +52,6 @@ import Approvals from './pages/Approvals';
 import Attendance from './pages/Attendance';
 import AttendanceReport from './pages/AttendanceReport';
 import Calendar from './pages/Calendar';
-import Dashboard from './pages/Dashboard';
 import Gallery from './pages/Gallery';
 import IDCards from './pages/IDCards';
 import Marks from './pages/Marks';
@@ -69,6 +68,7 @@ import StaffLogin from './pages/StaffLogin';
 import StaffManagement from './pages/StaffManagement';
 import Students from './pages/Students';
 import Teachers from './pages/Teachers';
+import Dashboard from './pages/Dashboard';
 import __Layout from './Layout.jsx';
 
 
@@ -78,7 +78,6 @@ export const PAGES = {
     "Attendance": Attendance,
     "AttendanceReport": AttendanceReport,
     "Calendar": Calendar,
-    "Dashboard": Dashboard,
     "Gallery": Gallery,
     "IDCards": IDCards,
     "Marks": Marks,
@@ -95,6 +94,7 @@ export const PAGES = {
     "StaffManagement": StaffManagement,
     "Students": Students,
     "Teachers": Teachers,
+    "Dashboard": Dashboard,
 }
 
 export const pagesConfig = {
