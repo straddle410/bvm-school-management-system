222293dd98fdf6803b38acfd54a0db4e6f698f09
Naresh G <straddle410@gmail.com>
2026-02-22 05:30:19 +0000

    File changes

--- functions/getStudentHallTickets.ts
+++ functions/getStudentHallTickets.ts
@@ -15,6 +15,11 @@ Deno.serve(async (req) => {
       '-created_date'
     );
 
+    // Fetch all exam types to resolve names
+    const examTypes = await base44.asServiceRole.entities.ExamType.list();
+    const examTypeMap = {};
+    examTypes.forEach(et => { examTypeMap[et.id] = et.name; });
+
     // For each hall ticket, fetch its exam timetable
     const ticketsWithTimetable = await Promise.all(
       hallTickets.map(async (ticket) => {
@@ -22,7 +27,8 @@ Deno.serve(async (req) => {
           { exam_type: ticket.exam_type, academic_year: ticket.academic_year },
           'exam_date'
         );
-        return { ...ticket, timetable };
+        const exam_type_name = examTypeMap[ticket.exam_type] || ticket.exam_type;
+        return { ...ticket, exam_type_name, timetable };
       })
     );
 
