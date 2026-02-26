ce56b802829b0beaa4acff82e39d20e99ffea060
Naresh G <straddle410@gmail.com>
2026-02-26 16:13:16 +0000

    File changes

--- src/pages/HallTicketManagement.jsx
+++ src/pages/HallTicketManagement.jsx
@@ -6,6 +6,7 @@ import TimetableManager from '@/components/hallTicket/TimetableManager';
 import HallTicketGenerator from '@/components/hallTicket/HallTicketGenerator';
 import HallTicketList from '@/components/hallTicket/HallTicketList';
 import ProgressCardsList from '@/components/hallTicket/ProgressCardsList';
+import AttendanceReportTab from '@/components/hallTicket/AttendanceReportTab';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { useAcademicYear } from '@/components/AcademicYearContext';
 
