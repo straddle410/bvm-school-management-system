eac9b1289950f4718091e20cb3e1f8e99d285d86
Naresh G <straddle410@gmail.com>
2026-02-28 12:14:36 +0000

    File changes

--- src/pages/StudentProfile.jsx
+++ src/pages/StudentProfile.jsx
@@ -304,7 +304,8 @@ export default function StudentProfile() {
               </CardContent>
             </Card>
           )}
-        </div>
-      </div>
-    );
-}
+              </TabsContent>
+            </Tabs>
+          ) : (
+            <>
+              {/* Quick Stats */}
