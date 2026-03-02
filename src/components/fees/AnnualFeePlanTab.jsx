522d63459f65015ccc23439b1c7f03f9800b8953
Naresh G <straddle410@gmail.com>
2026-03-02 11:41:37 +0000

    File changes

--- src/components/fees/AnnualFeePlanTab.jsx
+++ src/components/fees/AnnualFeePlanTab.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import FeePlanManager from './FeePlanManager';
 import GenerateInvoices from './GenerateInvoices';
+import InvoiceRegenerator from './InvoiceRegenerator';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 export default function AnnualFeePlanTab({ academicYear }) {
@@ -12,12 +13,16 @@ export default function AnnualFeePlanTab({ academicYear }) {
         <TabsList>
           <TabsTrigger value="plan">Set Fee Plan</TabsTrigger>
           <TabsTrigger value="generate">Generate Invoices</TabsTrigger>
+          <TabsTrigger value="regenerate">Regenerate Invoices</TabsTrigger>
         </TabsList>
         <TabsContent value="plan" className="pt-4">
           <FeePlanManager academicYear={academicYear} />
         </TabsContent>
         <TabsContent value="generate" className="pt-4">
           <GenerateInvoices academicYear={academicYear} />
+        </TabsContent>
+        <TabsContent value="regenerate" className="pt-4">
+          <InvoiceRegenerator academicYear={academicYear} />
         </TabsContent>
       </Tabs>
     </div>
