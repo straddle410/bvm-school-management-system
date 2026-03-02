064acf2594c17017d44319f298854f6e7f9e7caa
Naresh G <straddle410@gmail.com>
2026-03-02 17:15:42 +0000

    File changes

--- src/pages/Wave3ACollectionReport.jsx
+++ src/pages/Wave3ACollectionReport.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { base44 } from '@/api/base44Client';
 import { useQuery } from '@tanstack/react-query';
+import LoginRequired from '@/components/LoginRequired';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
