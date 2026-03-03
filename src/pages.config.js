b18c9497657533f4b76411c89d1107a162b5d185
Naresh G <straddle410@gmail.com>
2026-03-03 08:23:41 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -100,7 +100,7 @@ import SubjectManagement from './pages/SubjectManagement';
 import Teachers from './pages/Teachers';
 import TimetableManagement from './pages/TimetableManagement';
 import UserProfile from './pages/UserProfile';
-import ParentStatement from './pages/ParentStatement';
+import parentstatementStudentid from './pages/ParentStatement[studentId]';
 import __Layout from './Layout.jsx';
 
 
@@ -158,7 +158,7 @@ export const PAGES = {
     "Teachers": Teachers,
     "TimetableManagement": TimetableManagement,
     "UserProfile": UserProfile,
-    "ParentStatement": ParentStatement,
+    "ParentStatement[studentId]": parentstatementStudentid,
 }
 
 export const pagesConfig = {
