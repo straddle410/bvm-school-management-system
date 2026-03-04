6fdc1ff150a40ec5113d53e1652f9ad4d3f877b6
Naresh G <straddle410@gmail.com>
2026-03-04 09:00:52 +0000

    File changes

--- src/pages/Fees.jsx
+++ src/pages/Fees.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { base44 } from '@/api/base44Client';
 import LoginRequired from '@/components/LoginRequired';
 import { getStaffSession } from '@/components/useStaffSession';
 import { useAcademicYear } from '@/components/AcademicYearContext';
