a8e158b10128be70328e9d5ed080bbcd379cc714
Naresh G <straddle410@gmail.com>
2026-03-05 14:42:06 +0000

    File changes

--- src/pages/Marks.jsx
+++ src/pages/Marks.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { base44 } from '@/api/base44Client';
 import { createPageUrl } from '@/utils';
 import LoginRequired from '@/components/LoginRequired';
