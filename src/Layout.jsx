6e2de992ad47c058f461fc71076e02d0d678e041
Naresh G <straddle410@gmail.com>
2026-03-05 12:50:12 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -209,7 +209,9 @@ export default function Layout({ children, currentPageName }) {
 
       {/* Main Content */}
       <main className="flex-1 overflow-y-auto pb-20">
-        {children}
+        <StudentAuthGuard currentPageName={currentPageName}>
+          {children}
+        </StudentAuthGuard>
       </main>
 
       {/* Bottom Navigation */}
