e19fdcf1e2164db343e4dbd4e45d12a01e4dbb62
Naresh G <straddle410@gmail.com>
2026-03-05 12:09:23 +0000

    File changes

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -389,7 +389,7 @@ export default function Gallery() {
             </Button>
           </div>
         </DialogContent>
-        </Dialog>
-        </div>
-        );
-        }
+      </Dialog>
+      </div>
+      );
+      }
