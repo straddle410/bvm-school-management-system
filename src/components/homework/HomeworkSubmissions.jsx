b8c2613047f5fd50c5247af299733192d235406d
Naresh G <straddle410@gmail.com>
2026-03-08 07:50:58 +0000

    File changes

--- src/components/homework/HomeworkSubmissions.jsx
+++ src/components/homework/HomeworkSubmissions.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { base44 } from '@/api/base44Client';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import { X, Download, CheckCircle, Clock, FileText, AlertCircle } from 'lucide-react';
+import { X, Download, CheckCircle, Clock, FileText, AlertCircle, Pencil } from 'lucide-react';
 import { format } from 'date-fns';
 import { HOMEWORK_STATUS, normalizeHomeworkSubmissionStatus } from '@/components/utils/homeworkStatusHelper';
 import HomeworkDetailAnalyticsHeader from '@/components/homework/HomeworkDetailAnalyticsHeader';
