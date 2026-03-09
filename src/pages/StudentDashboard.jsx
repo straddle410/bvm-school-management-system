a11cdf3fe980f1f5ca9069f8a375109998247186
Naresh G <straddle410@gmail.com>
2026-03-09 08:00:07 +0000

    File changes

--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { base44 } from '@/api/base44Client';
 import { createPageUrl } from '@/utils';
 import { Link } from 'react-router-dom';
