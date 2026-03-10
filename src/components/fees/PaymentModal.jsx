e1c55259bb2ffcfe24c06dd331c5fbc0a652ee81
Naresh G <straddle410@gmail.com>
2026-03-10 17:21:17 +0000

    File changes

--- src/components/fees/PaymentModal.jsx
+++ src/components/fees/PaymentModal.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
+import { useState } from 'react';
 import { base44 } from '@/api/base44Client';
 import { useMutation } from '@tanstack/react-query';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
