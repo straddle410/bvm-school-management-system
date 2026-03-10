cb1c1c97e25e4568234678aa3c11ef1fe9c6280a
Naresh G <straddle410@gmail.com>
2026-03-10 05:48:39 +0000

    File changes

--- functions/createOrUpdateMarksWithValidation.ts
+++ functions/createOrUpdateMarksWithValidation.ts
@@ -13,11 +13,6 @@ function validateAcademicYearBoundary(date, academicYearStart, academicYearEnd) 
 Deno.serve(async (req) => {
   try {
     const base44 = createClientFromRequest(req);
-    const user = await base44.auth.me();
-
-    if (!user) {
-      return Response.json({ error: 'Unauthorized' }, { status: 401 });
-    }
 
     const {
       markData,
