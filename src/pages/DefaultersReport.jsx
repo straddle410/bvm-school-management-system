345d00fc2f7dd8e6c754a4b6f48ece4f8ef4e3c0
Naresh G <straddle410@gmail.com>
2026-03-14 08:15:22 +0000

    File changes

--- src/pages/DefaultersReport.jsx
+++ src/pages/DefaultersReport.jsx
@@ -15,8 +15,7 @@ import { useAcademicYear } from '@/components/AcademicYearContext';
 import DefaulterDetailDrawer from '@/components/fees/DefaulterDetailDrawer';
 
 export default function DefaultersReportPage() {
-  const { academicYear: contextAcademicYear } = useAcademicYear();
-  const academicYear = contextAcademicYear || '2025-26';
+  const { academicYear } = useAcademicYear();
   const [filters, setFilters] = useState({
     className: '',
     section: '',
