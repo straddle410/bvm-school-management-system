0e6d89d0fa1d12028f85592c83905a970803cfbb
Naresh G <straddle410@gmail.com>
2026-03-17 10:49:32 +0000

    File changes

--- src/components/fees/PaymentsList.jsx
+++ src/components/fees/PaymentsList.jsx
@@ -11,6 +11,7 @@ import { createPageUrl } from '@/utils';
 import { format, startOfWeek, startOfMonth } from 'date-fns';
 import VoidModal from './ReversalModal';
 import ReceiptPreviewModal from './ReceiptPreviewModal';
+import RecordPaymentModal from './RecordPaymentModal';
 
 const CLASSES = ['All', 'Nursery', 'LKG', 'UKG', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
 
@@ -288,6 +289,12 @@ export default function PaymentsList({ academicYear, isAdmin, canVoidReceipt }) 
         }}
         receiptData={receiptPreview}
         isLoading={isLoadingReceipt}
+      />
+
+      <RecordPaymentModal
+        isOpen={recordingNewPayment}
+        onClose={() => setRecordingNewPayment(false)}
+        academicYear={academicYear}
       />
     </div>
   );
