185c08e8e9020c492862a8195a262fe064887eb5
Naresh G <straddle410@gmail.com>
2026-03-17 16:42:47 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -82,6 +82,7 @@ function App() {
       <QueryClientProvider client={queryClientInstance}>
         <LoadingProvider>
           <AsyncOperationProvider>
+            <PullToRefreshDisabler />
             <Router>
               <TabHistoryProvider>
                 <NavigationTracker />
