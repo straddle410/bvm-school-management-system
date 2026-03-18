23865d45f1717a39dedbe536cd420dfd85546764
Naresh G <straddle410@gmail.com>
2026-03-18 11:28:40 +0000

    File changes

--- functions/getStudentsPaginated.ts
+++ functions/getStudentsPaginated.ts
@@ -1,4 +1,5 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.21';
 import { createHmac } from 'node:crypto';
 
 // Verify staff session token (same logic as getMyStaffProfile)
