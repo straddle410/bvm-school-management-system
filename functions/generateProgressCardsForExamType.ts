4be8d31e7c331dfa0249c4aef37b3fa174322cde
Naresh G <straddle410@gmail.com>
2026-03-18 05:32:26 +0000

    File changes

--- functions/generateProgressCardsForExamType.ts
+++ functions/generateProgressCardsForExamType.ts
@@ -1,6 +1,4 @@
-import { createClient } from 'npm:@base44/sdk@0.8.20';
-
-const base44 = createClient({ appId: Deno.env.get('BASE44_APP_ID') });
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 
 function validateAcademicYearBoundary(date, academicYearStart, academicYearEnd) {
   const d = new Date(date);
