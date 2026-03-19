d54bb0ffc9db6bd31c7a20281f2e59ec05545afb
Naresh G <straddle410@gmail.com>
2026-03-19 06:45:12 +0000

    File changes

--- src/pages/StudentProfile.jsx
+++ src/pages/StudentProfile.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { base44 } from '@/api/base44Client';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
