56e710de04e2212ef6f7b403e87e9f2d4092767d
Naresh G <straddle410@gmail.com>
2026-03-24 05:20:48 +0000

    File changes

--- src/components/PushNotificationManager.jsx
+++ src/components/PushNotificationManager.jsx
@@ -19,6 +19,8 @@ export default function PushNotificationManager() {
           appId: ONESIGNAL_APP_ID,
           notifyButton: { enable: false },
           allowLocalhostAsSecureOrigin: true,
+          serviceWorkerPath: '/api/functions/OneSignalSDKWorker',
+          serviceWorkerParam: { scope: '/' },
         });
 
         await OneSignal.Notifications.requestPermission();
